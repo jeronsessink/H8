7aa3e73eaad5e397dcaa4919c9ddf811338337c7
Jeron Sessink <48436071+jeronsessink@users.noreply.github.com>
2019-06-10 14:48:54 +0200

    Add files via upload

---
+++ objectenBeheren.java
@@ -0,0 +1,33 @@
+package OpdrachtB;
+import java.util.ArrayList;
+import java.util.Scanner; 
+import javax.swing.*;
+
+public class objectenBeheren {
+	public static void main(String[] args) {
+		//JFrame frame = new JFrame("My First GUI");
+	       //frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	      // frame.setSize(300,300);
+	      // frame.setVisible(true);
+		
+		@SuppressWarnings("resource")
+		Scanner keyboardInput = new Scanner(System.in);
+		
+	
+		ArrayList<String> auto = new ArrayList<String>();
+		auto.add("Porsche");
+		auto.add("Ferrari");
+		auto.add("Lamborghini");
+		
+		//hooi
+		 
+		
+		while (true) {
+		System.out.println("Welke auto wil je toevoegen?");
+		auto.add(keyboardInput.next());
+		
+		auto.forEach((n) -> System.out.println(n));
+		}
+		
+	}
+}
